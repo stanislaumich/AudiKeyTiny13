4f560c80cdc115b4317673b4b9f72a73b19d1076
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-09-21 13:19:44 +0300

    AUTO FROM WORK 21.09.2023 13:19:44,23

--- src/main.cpp
+++ src/main.cpp
@@ -32,3 +32,29 @@ int main() {
                 sleep_cpu(); // спать!
         }
 }
+https://4pda.to/forum/index.php?showtopic=953401&st=420
+https://soltau.ru/index.php/arduino/item/394-kak-sdelat-kopiyu-klyucha-dlya-domofona-v-domashnikh-usloviyakh
+#include <OneWire.h>
+OneWire iButton(10); // создаём объект 1-wire на 10 выводе
+
+void setup (void) {
+  Serial.begin(9600);
+}
+
+void loop(void) {
+  delay(1000); // задержка 1 сек
+  byte addr[8]; // массив для хранения данных ключа
+  
+  if ( !iButton.search(addr) ) { // если ключ не приложен
+      Serial.println("No key connected..."); // сообщаем об этом
+      return; // и прерываем программу
+  }
+  
+  Serial.print("Key : ");
+  for(int i=0; i<8; i++) {
+    Serial.print(addr[i], HEX); // выводим побайтно данные ключа
+    Serial.print(" ");
+  }
+  Serial.println();
+  iButton.reset(); // сброс ключа
+}
